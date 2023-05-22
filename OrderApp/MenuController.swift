90d5772ae8af9b67e30c125021f9087411138b77
SebasMac <ionrodriguezlara@gmail.com>
2023-05-22 03:45:13 -0700

    Finished functionality

--- OrderApp/MenuController.swift
+++ OrderApp/MenuController.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 class MenuController {
     
@@ -72,10 +73,25 @@ class MenuController {
         
         return orderResponse.prepTime
     }
+    
+    func fetchImage(from url: URL) async throws -> UIImage {
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            
+            throw MenuControllerError.imageDataMissing}
+        
+        guard let image = UIImage(data: data) else {
+            
+            throw MenuControllerError.imageDataMissing
+        }
+        
+        return image
+    }
 }
 
 enum MenuControllerError: Error, LocalizedError {
     case categoriesNotFound
     case menuItemsNotFound
     case orderRequestFailed
+    case imageDataMissing
 }
---
+++ OrderApp/OrderConfirmationViewController.swift
@@ -0,0 +1,40 @@
+//
+//  OrderConfirmationViewController.swift
+//  OrderApp
+//
+//  Created by Ion Sebastian Rodriguez Lara on 22/05/23.
+//
+
+import UIKit
+
+class OrderConfirmationViewController: UIViewController {
+
+    let minutesToPrepare: Int
+    @IBOutlet weak var confirmationLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        confirmationLabel.text = "Thank you for your order! Your wait time is approximately \(minutesToPrepare) minutes."
+    }
+
+    required init?(coder: NSCoder, minutesToPrepare: Int) {
+        self.minutesToPrepare = minutesToPrepare
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
